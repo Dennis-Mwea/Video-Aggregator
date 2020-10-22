ca863017b9d35bd807687ff195c2f42e8a6e543d
Dennis-Mwea <My mum and I1>
2020-10-22 08:55:00 +0300

    Show loading status

--- resources/views/livewire/coming-soon.blade.php
+++ resources/views/livewire/coming-soon.blade.php
@@ -1,19 +1,21 @@
 <div wire:init="loadComingSoonGames" class="most-anticipated-container space-y-10 mt-8">
-    @if(isset($comingSoon) && count($comingSoon) > 0)
-        @foreach($comingSoon as $game)
-            <div class="game flex">
-                <a href="#">
-                    <img src="{{ str_replace('thumb', 'cover_small', $game['cover']['url']) }}"
-                         alt="game cover"
-                         class="hover:opacity-75 transition ease-in-out duration-150 w-16">
-                </a>
-                <div class="ml-4">
-                    <a href="#" class="hover:text-gray-300">{{ $game['name'] }}</a>
-                    <p class="text-gray-400 text-sm mt-1">
-                        {{ \Carbon\Carbon::parse($game['first_release_date'])->format('M d, Y') }}
-                    </p>
-                </div>
+    @forelse($comingSoon as $game)
+        <div class="game flex">
+            <a href="#">
+                <img src="{{ str_replace('thumb', 'cover_small', $game['cover']['url']) }}"
+                     alt="game cover"
+                     class="hover:opacity-75 transition ease-in-out duration-150 w-16">
+            </a>
+            <div class="ml-4">
+                <a href="#" class="hover:text-gray-300">{{ $game['name'] }}</a>
+                <p class="text-gray-400 text-sm mt-1">
+                    {{ \Carbon\Carbon::parse($game['first_release_date'])->format('M d, Y') }}
+                </p>
             </div>
-        @endforeach
-    @endif
+        </div>
+    @empty
+        <div>
+            Loading...
+        </div>
+    @endforelse
 </div>
--- resources/views/livewire/recently-reviewed.blade.php
+++ resources/views/livewire/recently-reviewed.blade.php
@@ -1,42 +1,42 @@
 <div wire:init="loadRecentlyReviewed" class="recently-reviewed-container space-y-12 mt-8">
-    @if(isset($recentlyReviewed) && count($recentlyReviewed) > 0)
-        @foreach($recentlyReviewed as $game)
-            <div class="game bg-gray-800 rounded-lg shadow-md flex p-6">
-                <div class="relative inline-block flex-none">
-                    <a href="#">
-                        <img src="{{ str_replace('thumb', 'cover_big', $game['cover']['url']) }}"
-                             alt="game cover"
-                             class="hover:opacity-75 transition ease-in-out duration-150 w-20 lg:w-48">
-                    </a>
+    @forelse$recentlyReviewed as $game)
+    <div class="game bg-gray-800 rounded-lg shadow-md flex p-6">
+        <div class="relative inline-block flex-none">
+            <a href="#">
+                <img src="{{ str_replace('thumb', 'cover_big', $game['cover']['url']) }}"
+                     alt="game cover"
+                     class="hover:opacity-75 transition ease-in-out duration-150 w-20 lg:w-48">
+            </a>
 
-                    @if(array_key_exists('rating', $game))
-                        <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full"
-                             style="right: -20px; bottom: -20px">
-                            <div class="font-semibold text-xs flex justify-center items-center h-full">
-                                {{ round($game['rating'], 0) }}%
-                            </div>
-                        </div>
-                    @endif
-                </div>
-
-                <div class="ml-6 lg:ml-12">
-                    <a href="#"
-                       class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-0 lg:mt-4">
-                        {{ $game['name'] }}
-                    </a>
-                    <div class="text-gray-400 mt-1">
-                        @foreach($game['platforms'] as $platform)
-                            @if (array_key_exists('abbreviation', $platform))
-                                {{ $platform['abbreviation'] }},
-                            @endif
-                        @endforeach
+            @if(array_key_exists('rating', $game))
+                <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full"
+                     style="right: -20px; bottom: -20px">
+                    <div class="font-semibold text-xs flex justify-center items-center h-full">
+                        {{ round($game['rating'], 0) }}%
                     </div>
-
-                    <p class="mt-6 text-gray-400  hidden lg:block">
-                        {{ $game['summary'] }}
-                    </p>
                 </div>
+            @endif
+        </div>
+
+        <div class="ml-6 lg:ml-12">
+            <a href="#"
+               class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-0 lg:mt-4">
+                {{ $game['name'] }}
+            </a>
+            <div class="text-gray-400 mt-1">
+                @foreach($game['platforms'] as $platform)
+                    @if (array_key_exists('abbreviation', $platform))
+                        {{ $platform['abbreviation'] }},
+                    @endif
+                @endforeach
             </div>
-        @endforeach
-    @endif
+
+            <p class="mt-6 text-gray-400  hidden lg:block">
+                {{ $game['summary'] }}
+            </p>
+        </div>
+    </div>
+    @empty
+        <div>Loading...</div>
+    @endforelse
 </div>
