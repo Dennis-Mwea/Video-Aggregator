e6f6f405a7475ac747206ac89a5cf16db8451373
Dennis-Mwea <My mum and I1>
2020-10-22 09:42:14 +0300

    Remove unused spinner package

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,6 @@ module.exports = {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
     ],
-
     theme: {
         extend: {
             fontFamily: {
@@ -16,29 +15,11 @@ module.exports = {
                 '44': '11rem'
             }
         },
-
-        spinner: (theme) => ({
-            default: {
-                color: '#dae1e7', // color you want to make the spinner
-                size: '1em', // size of the spinner (used for both width and height)
-                border: '2px', // border-width of the spinner (shouldn't be bigger than half the spinner's size)
-                speed: '500ms', // the speed at which the spinner should rotate
-            },
-            // md: {
-            //   color: theme('colors.red.500', 'red'),
-            //   size: '2em',
-            //   border: '2px',
-            //   speed: '500ms',
-            // },
-        }),
     },
-
     variants: {
         opacity: ['responsive', 'hover', 'focus', 'disabled'],
     },
-
     plugins: [
         require('@tailwindcss/ui'),
-        require('tailwindcss-spinner')()
     ],
 };
